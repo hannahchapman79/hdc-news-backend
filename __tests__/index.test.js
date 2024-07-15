cc6a4b467408505472b3d10a3d95db4f4690f3dc
hannahchapman79 <hannahchapman1999@aol.com>
2024-07-15 12:26:56 +0100

    set up the project and complete get request for /api/topics endpoint

---
+++ __tests__/index.test.js
@@ -0,0 +1,33 @@
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed")
+const request = require('supertest')
+const app = require("../app")
+const data = require('../db/data/test-data/index')
+
+beforeEach(() => {
+   return seed(data) 
+})
+
+afterAll(() => {
+    return db.end()
+})
+
+describe("/api/topics", () => {
+    test("responds with all topics", () => {
+        return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((response) => {
+            expect(response.body.topics.length).toBe(3);
+            response.body.topics.forEach((topic) => {
+              expect(typeof topic.description).toBe('string');
+              expect(typeof topic.slug).toBe('string');
+            })
+        })
+    })
+    test("responds with all topics", () => {
+        return request(app)
+        .get("/api/topicsss")
+        .expect(404)
+    })
+});
---
+++ db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+const ENV = process.env.NODE_ENV || 'development';
+
+const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
+
+
+
+require('dotenv').config({
+  path: pathToCorrectEnvFile,
+});
+
+module.exports = new Pool();
