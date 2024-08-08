62e6d6391bb56b13f6b3a46350e235e42a777357
hannahchapman79 <hannahchapman1999@aol.com>
2024-08-08 12:04:47 +0100

    updates repo to treat comment_count as a sortable column

--- models/articles.models.js
+++ models/articles.models.js
@@ -62,27 +62,30 @@ exports.selectArticles = (sort_by = "created_at", order = "desc", topic) => {
 
   else {
     let sqlString = `
-  SELECT
-  articles.article_id, 
-  articles.title, 
-  articles.topic, 
-  articles.author, 
-  articles.created_at, 
-  articles.votes, 
-  articles.article_img_url,
-  COUNT(comments.comment_id) AS comment_count
-  FROM 
-  articles
-  LEFT JOIN 
-  comments
-  ON 
-  articles.article_id = comments.article_id`
+    WITH article_counts AS (
+      SELECT
+        articles.article_id,
+        articles.title,
+        articles.topic,
+        articles.author,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+      FROM 
+        articles
+      LEFT JOIN 
+        comments ON articles.article_id = comments.article_id
+      GROUP BY 
+        articles.article_id
+    )
+    SELECT * FROM article_counts`
 
   if (topic) {
-    sqlString += ` WHERE articles.topic = '${topic}'`;
+    sqlString += ` WHERE topic = '${topic}'`;
   }
 
-  sqlString += ` GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order};`;
+  sqlString += ` ORDER BY ${sort_by} ${order};`;
 
     return db.query(sqlString).then((result) => {
       if (result.rows.length === 0) {
